9a00b7d1cd4c8f82c03e068802b260dec2a8611a
rlawndks4 <rlawndks0423@naver.com>
2024-03-29 03:23:29 +0900

    update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(cors());
 
 const https = require('https');
 const http = require('http');
+const { processSocket } = require('./util/socket');
 
 const PORT = 5000;
 
@@ -38,17 +39,7 @@ io.on('connection', (socket) => {
         const {
             method, data, brand_id, title
         } = msg;
-        let method_list = [
-            `deposit`,
-            'settle_request',
-            'withdraw_request',
-            'settle_plus'
-        ]
-        if (method_list.includes(method)) {
-            io.emit('message', {
-                method, data, brand_id, title
-            });
-        }
+        processSocket(msg, io)
     });
 
     socket.on('disconnect', () => {
